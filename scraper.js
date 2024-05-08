d4e55e9bd44d07fe261a2bc3bf76399a593fd02f
Santimaster13 <santiagomercadob@gmail.com>
2024-05-08 16:39:07 -0500

    Alkosto ready
    Ya se consiguen los 3 productos de alkosto, se tiene la lista ordenada por precio utilizando la variable priceint y se mejoró el manejo de errores.

--- scraper.js
+++ scraper.js
@@ -1,6 +1,8 @@
 const { query } = require('express');
 const { chromium } = require('playwright');
 
+
+//Funciones main de scrapping
 const scrapeMercadoLibre = async (productName) => {
 let index = 0;
 let count = 0;
@@ -17,10 +19,38 @@ while(keepsearching==true & count < 3) {
          }
      }
      index++;
+     if(index-count > 4){
+        keepsearching = false;
+    }
 }
     return productos;
 };
 
+const scrapeAlkosto = async (productName) => {
+    let index = 0;
+    let count = 0;
+    let keepsearching = true;
+    let productos = [];
+    while(keepsearching==true & count < 3) {
+         const product = await getAlkproduct(productName, index);
+         if(product !== null){
+             if(product.found == false){
+                keepsearching = false;
+             } else {
+               productos.push(product);
+               count++;
+             }
+         }
+         index++;
+         if(index-count > 4){
+            keepsearching = false;
+        }
+    }
+        return productos;
+    };
+
+
+//Funciones de scrapping para un producto    
 const getMLproduct = async (productName, prodindex) => {
     const browser = await chromium.launch({
         headless: false,
@@ -28,30 +58,36 @@ const getMLproduct = async (productName, prodindex) => {
     });
 
    let found = false;
-    const page = await browser.newPage();
-    await page.goto('https://www.mercadolibre.com.co');
-    await page.waitForLoadState('domcontentloaded');
+   const page = await browser.newPage();
+   await page.goto('https://www.mercadolibre.com.co');
+   await page.waitForLoadState('domcontentloaded');
 
-    // Ingresa el nombre del producto en el campo de búsqueda y envía la búsqueda
-    await page.click('#cb1-edit');
-    await page.fill('#cb1-edit', productName);
-    await page.press('#cb1-edit', 'Enter');
-    await page.waitForLoadState('domcontentloaded');
-
-    // Filtrar por precio y productos nuevos
-    await page.click('#\\:R2m55e6\\:\\-display\\-values');
-    await page.click('#\\:R2m55e6\\:\\-menu\\-list\\-option\\-price_asc');
-    await page.waitForLoadState('domcontentloaded');
-    const filtronuevo = await page.waitForSelector("span.ui-search-filter-name:text('Nuevo')", { timeout: 3000 });
-    if (filtronuevo) {
-        await filtronuevo.click();
+   // Ingresa el nombre del producto en el campo de búsqueda y envía la búsqueda
+   await page.click('#cb1-edit');
+   await page.fill('#cb1-edit', productName);
+   await page.press('#cb1-edit', 'Enter');
+   await page.waitForLoadState('domcontentloaded');
+    try {
+        // Filtrar por precio y productos nuevos
+        await new Promise(resolve => setTimeout(resolve, 500));
+        await page.click('#\\:R2m55e6\\:\\-display\\-values');
+        await page.click('#\\:R2m55e6\\:\\-menu\\-list\\-option\\-price_asc');
+        await page.waitForLoadState('domcontentloaded');
+        await new Promise(resolve => setTimeout(resolve, 500));
+        // Filtrar por productos nuevos
+        const filtronuevo = await page.waitForSelector("span.ui-search-filter-name:text('Nuevo')", { timeout: 2000 });
+        await filtronuevo.click(); 
+    } catch (error) {
+        await browser.close();
+        console.log("Error en el producto " + prodindex + " de Mercado Libre, intentando con el siguiente...");
+        return null;
     }   
-
-    // Seleccionar el primer producto de la lista
-    await page.waitForLoadState('domcontentloaded');
+        // Seleccionar el primer producto de la lista
+        await page.waitForLoadState('domcontentloaded');
     const items = await page.$$('.ui-search-item__group__element.ui-search-link__title-card.ui-search-link');
     if (items.length > prodindex) {
         await items[prodindex].click();
+        await page.waitForLoadState('domcontentloaded');
         // Extraer los datos del producto
 
         // Título
@@ -60,15 +96,22 @@ const getMLproduct = async (productName, prodindex) => {
             title = await page.$eval('.ui-pdp-title', element => element.innerText.trim());
         } catch (error) {
             await browser.close();
+            console.log("Error en el producto " + prodindex + " de Mercado Libre, intentando con el siguiente...");
             return null;
         }
 
         // Precio
         let price;
         try {
-            price = await page.$eval('.ui-pdp-price__main-container', element => element.innerHTML);
+            price = await page.$eval('.ui-pdp-price__second-line', element => element.innerText.trim());
+            let lineas = price.split('\n');
+            price = lineas[1];
+            priceint = parseInt(price.replaceAll('.', ''));
+            price = "$"+price;
+            console.log(priceint);
         } catch (error) {
             await browser.close();
+            console.log("Error en el producto " + prodindex + " de Mercado Libre, intentando con el siguiente...");
             return null;
         }
 
@@ -78,6 +121,7 @@ const getMLproduct = async (productName, prodindex) => {
             image = await page.$eval('.ui-pdp-gallery__figure', element => element.innerHTML);
         } catch (error) {
                 await browser.close();
+                console.log("Error en el producto " + prodindex + " de Mercado Libre, intentando con el siguiente...");
                 return null;
             }
 
@@ -93,6 +137,7 @@ const getMLproduct = async (productName, prodindex) => {
             description = await page.$eval('p.ui-pdp-description__content', element => element.innerHTML);
         } catch (error) {
             await browser.close();
+            console.log("Error en el producto " + prodindex + " de Mercado Libre, intentando con el siguiente...");
             return null;
         }
 
@@ -106,6 +151,7 @@ const getMLproduct = async (productName, prodindex) => {
                 specifications = await page.$eval('.ui-vpp-highlighted-specs__attribute-columns', element => element.innerText.trim());
             } catch (error) {
                 await browser.close();
+                console.log("Error en el producto " + prodindex + " de Mercado Libre, intentando con el siguiente...");
                 return null;
             }
             
@@ -114,11 +160,117 @@ const getMLproduct = async (productName, prodindex) => {
         found = true;
         await browser.close();
         // Retorna los datos del producto
-        return { title, price, image, description, specifications, url, found };
+        return { title, price, image, description, specifications, url, found, priceint };
     }    else {
             await browser.close();
             return { found };
     }
 };
 
-module.exports = { scrapeMercadoLibre };
+const getAlkproduct = async (productName, prodindex) => {
+    const browser = await chromium.launch({
+        headless: false,
+        slowMo: 500
+    });
+
+   let found = false;
+    const page = await browser.newPage();
+    await page.goto('https://www.alkosto.com/', { timeout: 60000 }); // Aumentamos el tiempo de espera a 60 segundos
+    await page.waitForLoadState('domcontentloaded');
+
+    // Ingresa el nombre del producto en el campo de búsqueda y envía la búsqueda
+    await page.click('#js-site-search-input');
+    await page.fill('#js-site-search-input', productName);
+    await page.press('#js-site-search-input', 'Enter');
+    await page.waitForLoadState('domcontentloaded'); 
+
+    // Filtrar por precio y productos nuevos
+    await page.click('#sort-by');  
+    sortprice = await page.waitForSelector('.js-custom-option[data-value="alkostoIndexAlgoliaPRD_price_asc"]', { timeout: 3000 });
+    await new Promise(resolve => setTimeout(resolve, 700));
+    sortprice.click();
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    // Seleccionar el primer producto de la lista
+    await page.waitForLoadState('domcontentloaded');
+    await new Promise(resolve => setTimeout(resolve, 500));
+    const items = await page.$$('.product__item__top__title.js-algolia-product-click.js-algolia-product-title');
+    if (items.length > prodindex) {
+        await items[prodindex].click();
+        await page.waitForLoadState('domcontentloaded');
+        // Extraer los datos del producto
+
+        // Título
+        let title;
+        try{
+            title = await page.$eval('.new-container__header__title', element => element.innerText.trim()); 
+        } catch (error) {
+            await browser.close();
+            console.log("Error en el producto " + prodindex + " de Alkosto, intentando con el siguiente...");
+            return null;
+        }
+
+        // Precio
+        let price;
+        try {
+            price = await page.$eval('#js-original_price', element => element.innerText.trim());
+            price = price.replace(/\s/g, '');
+            price = price.replace('Hoy', '');
+            priceint = parseInt(price.replace('$', '').replaceAll('.', ''));
+            console.log(priceint);
+        } catch (error) {
+            await browser.close();
+            console.log("Error en el producto " + prodindex + " de Alkosto, intentando con el siguiente...");
+            return null;
+        }
+
+        // Imagen
+        let image;
+        try{
+            image = await page.$eval('.owl-lazy.js-zoom-desktop-new', element => element.getAttribute('src'));
+            image = "https://www.alkosto.com" + image;
+            image = "<img src='" + image + "' alt='Imagen del producto'>";
+        } catch (error) {
+                await browser.close();
+                console.log("Error en el producto " + prodindex + " de Alkosto, intentando con el siguiente...");
+                return null;
+            }
+
+        //Descripción
+        let description;
+        try {
+            description = await page.$eval('#wc-product-characteristics', element => element.innerHTML);
+            description = "<p>" + description + "</p>";
+        } catch (error) {
+            await browser.close();
+            console.log("Error en el producto " + prodindex + " de Alkosto, intentando con el siguiente...");
+            return null;
+        }
+
+        //Especificaciones
+        let specifications;
+        try {
+            specifications = await page.$eval('.tab-details__keyFeatures--list', element => element.innerHTML);
+            specifications = "<ul>" + specifications + "</ul>";
+        } catch (error) {
+            try{
+                specifications = await page.$eval('.new-container__table__classifications___type__wrap.new-container__table__classifications___type__wrap--mobile', element => element.innerText.trim());
+            } catch (error) {
+                await browser.close();
+                console.log("Error en el producto " + prodindex + " de Alkosto, intentando con el siguiente...");
+                return null;
+            }
+            
+        }
+        const url = page.url();
+        found = true;
+        await browser.close();
+        // Retorna los datos del producto
+        return { title, price, image, description, specifications, url, found, priceint };
+    }    else {
+            await browser.close();
+            return { found };
+    }
+};
+
+module.exports = { scrapeMercadoLibre, scrapeAlkosto };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { scrapeMercadoLibre } = require('./scraper'); // Asegúrate de que el nombre del archivo sea correcto
+const { scrapeMercadoLibre, scrapeAlkosto } = require('./scraper'); // Asegúrate de que el nombre del archivo sea correcto
 
 const app = express();
 
@@ -11,15 +11,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true })); // Middleware para procesar datos del formulario
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'PriceHive', contentAmazon: null});
+    res.render('index', { title: 'PriceHive', content: null});
 });
 
 app.post('/search', async (req, res) => {
     const productName = req.body.productName; // Obtiene el texto de la barra de búsqueda
     try {
-        contentAmazon = await scrapeMercadoLibre(productName); // Llama a la función scrapeAmazon con el producto buscado
-        console.log(contentAmazon);
-        res.render('index', { title: 'Tienda', contentAmazon });
+        contentML = await scrapeMercadoLibre(productName); // Llama a la función scrapeMercadoLibre con el producto buscado
+        contentAlk = await scrapeAlkosto(productName); // Llama a la función scrapeAlkosto con el producto buscado
+        content = contentAlk.concat(contentML); // Concatena los resultados de las tiendas
+        let contentsorted = content;
+        contentsorted.sort(function(a, b) {
+            return a.priceint - b.priceint; //Ordenar por precio para el filtro
+        });
+        console.log(contentsorted);
+        res.render('index', { title: 'Tienda', contentsorted });
     } catch (error) {
         console.error('Failed to scrape Amazon:', error);
         res.send('Error scraping Amazon');
